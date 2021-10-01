7572bd0367a165d04347a9fb6013ea5aa314553c
ArthurFerreira2 <arthurferreira2@gmail.com>
2021-10-01 16:57:05 +0200

    simplifications

--- asm-tests/assemble.sh
+++ asm-tests/assemble.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-
 riscv32-unknown-elf-gcc -nostdlib -nostartfiles -T link.ld  $1.s -o $1.elf
-
-# riscv32-unknown-elf-as -march=rv32i -mabi=ilp32 -march-attr -fpic -R -no-pad-sections $1.s -o $1.o
-# riscv32-unknown-elf-ld $1.o -o $1.elf
-
 riscv32-unknown-elf-objcopy $1.elf -O binary $1.bin
 riscv32-unknown-elf-objdump -d $1.elf > $1.dump
